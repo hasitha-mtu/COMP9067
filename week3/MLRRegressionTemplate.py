a0391b5e89e118be5749fd9e5257f213619b7393
Hasitha Adikari <adikari.adikari@mycit.ie>
2025-02-04 16:16:28 +0000

    lab update

--- week3/MLRRegressionTemplate.py
+++ week3/MLRRegressionTemplate.py
@@ -12,11 +12,12 @@ import pandas as pd
 # coefficient and add the bias. It should then return the predicted 
 # y values
 def hypothesis(X, coefficients, bias):
-    
+    print(f'hypothesis|X shape: {X.shape}')
+    print(f'hypothesis|coefficients shape: {coefficients.shape}')
     # TODO: Calculate and return predicted results
-
-    
-    return 0
+    y = (X @ coefficients.reshape(-1, 1)) + bias
+    print(f'hypothesis|y shape: {y.shape}')
+    return y
 
 
 
@@ -36,20 +37,50 @@ def calculateRSquared(bias, coefficients,X, Y):
     
 
 
-        
-    
+# Complete the gradient calculations in the gradient_descent function. The function calculates
+# the gradient for the bias and the updated bias value. You should also update each of the
+# coefficients using the same gradient descent update rule. Calculate the gradient and use this
+# to update the coefficient. You will need one for loop for iterating with gradient descent and
+# in this basic implementation you can use a second (inner) for loop for updating the value of
+# each of the weights (coefficients). Once complete you should now be able to run your MLR
+# algorithm
+# def gradient_descent(X, y, learning_rate=0.01, n_iterations=1000):
+#     m, n = X.shape  # m: number of samples, n: number of features
+#     X = np.concatenate((np.ones((m, 1)), X), axis=1)  # Add bias term to X
+#     b = np.zeros(n + 1)  # Initialize coefficients and bias to 0
+#
+#     for _ in range(n_iterations):
+#         y_hat = X @ b  # Predictions
+#         error = y_hat - y  # Error
+#
+#         db = (2 / m) * np.sum(error)  # Gradient of bias
+#         dw = (2 / m) * (X.T @ error)  # Gradient of coefficients
+#
+#         b[0] = b[0] - learning_rate * db  # Update bias
+#         b[1:] = b[1:] - learning_rate * dw[1:]  # Update coefficients
+#
+#     return b  # Return the learned coefficients and bias
 
 def gradient_descent(bias, coefficients, alpha, X, Y, max_iter):
-
-
+    m, n = X.shape
     length = len(X)
     
     # array is used to store change in cost function for each iteration of GD
     errorValues = []
     
     for num in range(0, max_iter):
-        
-        # TODO: 
+        y_pred = hypothesis(X, coefficients, bias)
+        error = y_pred - Y
+        errorValues.append(error)
+        db = (2 / m) * np.sum(error)
+        dw = (2 / m) * (X.T @ error)
+        print(f'gradient_descent|dw shape: {dw.shape}')
+        print(f'gradient_descent|coefficients shape: {coefficients.shape}')
+        print(f'gradient_descent|alpha: {alpha}')
+
+        bias = bias - alpha * db
+        coefficients = coefficients - alpha * dw
+        # TODO:
         # Calculate predicted y values for current coefficient and bias values 
         # calculate and update bias using gradient descent rule
         # Update each coefficient value in turn using gradient descent rule
@@ -69,14 +100,15 @@ def gradient_descent(bias, coefficients, alpha, X, Y, max_iter):
 
 # Complete this function for part 2 of the exercise. 
 
-# def calculateTestR2(bias, coefficients, testFile):
-    
+def calculateTestR2(bias, coefficients, testFile):
+    pass
 
 def multipleLinearRegression(X, Y):
 
     # set the number of coefficients (weights) equal to the number of features
     #complete this line of code
-    #coefficients = ????
+    coefficients = np.random.rand(X.shape[1])
+    print(f'coefficients shape: {coefficients.shape}')
     
     bias = 0.0
     
@@ -102,7 +134,9 @@ def main():
      
     # Seperate teh features from the target feature    
     Y = data[:, -1]
+    print(f'Y shape: {Y.shape}')
     X = data[:, :-1]
+    print(f'X shape: {X.shape}')
     
     # Standardize each of the features in the dataset. 
     for num in range(len(X[0])):
@@ -120,4 +154,5 @@ def main():
 
     
 
-main()
+if __name__ == "__main__":
+    main()
